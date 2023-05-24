4513fa827bbf42d167bb534eba8a44a619f8c7f8
ekert-it <34439886+ekert-it@users.noreply.github.com>
2023-05-24 15:50:37 +0000

    Einige Dateinamen optimieren + Beispiel

---
+++ py/01_python_pur.py
@@ -0,0 +1,5 @@
+# Wir verwenden im Kurs Jupyter Notebooks, da sich diese für den Unterricht gut eignen.
+# Programme in Python werden in .py Dateien erstellt.
+# Sie können hier einfach loslesen und dann die Datei ausführen:
+
+print ("Los geht's")
